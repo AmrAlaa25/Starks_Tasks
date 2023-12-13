09d647c771cdd121bb9b78d017c04330ade9acbd
amr alaa <amra33226@gmail.com>
2023-12-13 23:28:05 +0200

    Week_1

---
+++ week_1/Task4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+float num_1,num_2;
+	printf("Type the frist number:");
+	scanf("%f",&num_1);
+	printf("Type the second number:");
+	scanf("%f",&num_2);
+	printf("the sum=%0.1f\n",num_1+num_2);
+	printf("the sub=%0.1f\n",num_1-num_2);	
+	printf("the Multi=%0.1f\n",num_1*num_2);
+printf("the division=%0.1f",num_1/num_2);
+}
